3c886659ddabcde4ba7379e33f10e747537b8753
Casimir Koenderink <casimir@Kapitein-Biet.local>
2026-09-18 22:39:01 +0200

    Dishes for Asia, Middle-East and Europe added

--- recepten.js
+++ recepten.js
@@ -173,7 +173,429 @@ const recepten = {
         korteBeschrijving: "Knapperige Filipijnse loempia's gevuld met groenten, vlees of garnalen.",
         afbeelding: "images/recepten/LumpiaAI.png",
         url: "recepten.html?id=lumpia"
-    }
+    },
+
+        borsjtsj: {
+        titel: "Borsjtsj",
+        korteBeschrijving: "Dieprode bietensoep met groenten, vaak geserveerd met zure room en verse kruiden.",
+        afbeelding: "images/recepten/BorsjtsjAI.png",
+        url: "recepten.html?id=borsjtsj"
+    },
+
+    pelmeni: {
+        titel: "Pelmeni",
+        korteBeschrijving: "Siberische deegkussentjes gevuld met gekruid vlees, traditioneel geserveerd met zure room.",
+        afbeelding: "images/recepten/PelmeniAI.png",
+        url: "recepten.html?id=pelmeni"
+    },
+
+    blini: {
+        titel: "Blini",
+        korteBeschrijving: "Dunne Oost-Slavische pannenkoekjes met hartige of zoete toppings.",
+        afbeelding: "images/recepten/BliniAI.png",
+        url: "recepten.html?id=blini"
+    },
+
+    khorkhog: {
+        titel: "Khorkhog",
+        korteBeschrijving: "Mongoolse vleesschotel met groenten, traditioneel gegaard met gloeiendhete stenen.",
+        afbeelding: "images/recepten/KhorkhogAI.png",
+        url: "recepten.html?id=khorkhog"
+    },
+
+    plov: {
+        titel: "Plov",
+        korteBeschrijving: "Centraal-Aziatisch rijstgerecht met mals vlees, wortel, ui en aromatische specerijen.",
+        afbeelding: "images/recepten/PlovAI.png",
+        url: "recepten.html?id=plov"
+    },
+
+    lagman: {
+        titel: "Lagman",
+        korteBeschrijving: "Stevige noedelschotel met vlees, paprika, tomaat en groenten in een kruidige saus.",
+        afbeelding: "images/recepten/LagmanAI.png",
+        url: "recepten.html?id=lagman"
+    },
+
+    manty: {
+        titel: "Manty",
+        korteBeschrijving: "Gestoomde deegpakketjes gevuld met gekruid vlees en ui, vaak geserveerd met yoghurt.",
+        afbeelding: "images/recepten/MantyAI.png",
+        url: "recepten.html?id=manty"
+    },
+
+    shashlik: {
+        titel: "Shashlik",
+        korteBeschrijving: "Gegrilde vleesspiesjes, gemarineerd met ui en kruiden en geroosterd boven open vuur.",
+        afbeelding: "images/recepten/ShashlikAI.png",
+        url: "recepten.html?id=shashlik"
+    },
+
+    biryani: {
+        titel: "Biryani",
+        korteBeschrijving: "Aromatische rijstschotel met specerijen, saffraan en mals vlees of groenten.",
+        afbeelding: "images/recepten/BiryaniAI.png",
+        url: "recepten.html?id=biryani"
+    },
+
+    masalaDosa: {
+        titel: "Masala Dosa",
+        korteBeschrijving: "Knapperige Zuid-Indiase pannenkoek van rijst en linzen, gevuld met gekruide aardappel.",
+        afbeelding: "images/recepten/MasalaDosaAI.png",
+        url: "recepten.html?id=masalaDosa"
+    },
+
+    momo: {
+        titel: "Momo",
+        korteBeschrijving: "Gestoomde Himalaya-dumplings, gevuld met gekruid vlees of groenten en geserveerd met saus.",
+        afbeelding: "images/recepten/MomoAI.png",
+        url: "recepten.html?id=momo"
+    },
+
+    kottuRoti: {
+        titel: "Kottu Roti",
+        korteBeschrijving: "Sri Lankaanse roerbakschotel van fijngehakte roti, groenten, ei en kruidige smaakmakers.",
+        afbeelding: "images/recepten/KottuRotiAI.png",
+        url: "recepten.html?id=kottuRoti"
+    },
+
+    khachapuri: {
+        titel: "Khachapuri",
+        korteBeschrijving: "Georgisch brood gevuld met gesmolten kaas, vaak geserveerd met boter of ei.",
+        afbeelding: "images/recepten/KhachapuriAI.png",
+        url: "recepten.html?id=khachapuri"
+    },
+
+    khinkali: {
+        titel: "Khinkali",
+        korteBeschrijving: "Georgische dumplings gevuld met gekruid vlees en aromatische bouillon.",
+        afbeelding: "images/recepten/KhinkaliAI.png",
+        url: "recepten.html?id=khinkali"
+    },
+
+    dolma: {
+        titel: "Dolma",
+        korteBeschrijving: "Gevulde wijnbladeren of groenten met rijst, kruiden en soms vlees.",
+        afbeelding: "images/recepten/DolmaAI.png",
+        url: "recepten.html?id=dolma"
+    },
+
+    mtsvadi: {
+        titel: "Mtsvadi",
+        korteBeschrijving: "Georgische vleesspiesjes, gegrild boven houtskool en geserveerd met ui en kruiden.",
+        afbeelding: "images/recepten/MtsvadiAI.png",
+        url: "recepten.html?id=mtsvadi"
+    }, 
+
+    kebab: {
+        titel: "Kebab",
+        korteBeschrijving: "Gegrild vlees, kruidig gemarineerd en geserveerd met brood, rijst of gegrilde groenten.",
+        afbeelding: "images/recepten/KebabAI.png",
+        url: "recepten.html?id=kebab"
+    },
+
+    manti: {
+        titel: "Mantı",
+        korteBeschrijving: "Kleine Turkse deegkussentjes gevuld met vlees, geserveerd met yoghurt en gekruide boter.",
+        afbeelding: "images/recepten/MantiAI.png",
+        url: "recepten.html?id=manti"
+    },
+
+    lahmacun: {
+        titel: "Lahmacun",
+        korteBeschrijving: "Dunne knapperige deegbodem belegd met gekruid gehakt, tomaat, ui en kruiden.",
+        afbeelding: "images/recepten/LahmacunAI.png",
+        url: "recepten.html?id=lahmacun"
+    },
+
+    menemen: {
+        titel: "Menemen",
+        korteBeschrijving: "Turks eiergerecht met tomaat, paprika en kruiden, zacht en sappig bereid.",
+        afbeelding: "images/recepten/MenemenAI.png",
+        url: "recepten.html?id=menemen"
+    },
+
+    shawarma: {
+        titel: "Shawarma",
+        korteBeschrijving: "Dun gesneden gekruid vlees, vaak geserveerd in platbrood met saus en groenten.",
+        afbeelding: "images/recepten/ShawarmaAI.png",
+        url: "recepten.html?id=shawarma"
+    },
+
+    falafel: {
+        titel: "Falafel",
+        korteBeschrijving: "Krokante balletjes van kikkererwten of tuinbonen, gekruid met verse kruiden en specerijen.",
+        afbeelding: "images/recepten/FalafelAI.png",
+        url: "recepten.html?id=falafel"
+    },
+
+    hummus: {
+        titel: "Hummus",
+        korteBeschrijving: "Romige puree van kikkererwten, tahin, citroen en knoflook, vaak gegeten met platbrood.",
+        afbeelding: "images/recepten/HummusAI.png",
+        url: "recepten.html?id=hummus"
+    },
+
+    maqluba: {
+        titel: "Maqluba",
+        korteBeschrijving: "Gelaagde rijstschotel met groenten en vlees, omgekeerd geserveerd als feestelijk geheel.",
+        afbeelding: "images/recepten/MaqlubaAI.png",
+        url: "recepten.html?id=maqluba"
+    },
+
+    chelowKebab: {
+        titel: "Chelow Kebab",
+        korteBeschrijving: "Perzische kebab geserveerd met geurige saffraanrijst, boter en gegrilde tomaat.",
+        afbeelding: "images/recepten/ChelowKebabAI.png",
+        url: "recepten.html?id=chelowKebab"
+    },
+
+    fesenjan: {
+        titel: "Fesenjan",
+        korteBeschrijving: "Rijke stoofschotel met walnoten en granaatappel, vaak bereid met kip of eend.",
+        afbeelding: "images/recepten/FesenjanAI.png",
+        url: "recepten.html?id=fesenjan"
+    },
+
+    ghormehSabzi: {
+        titel: "Ghormeh Sabzi",
+        korteBeschrijving: "Kruidige Perzische stoofpot met verse kruiden, bonen en langzaam gegaard vlees.",
+        afbeelding: "images/recepten/GhormehSabziAI.png",
+        url: "recepten.html?id=ghormehSabzi"
+    },
+
+    ashReshteh: {
+        titel: "Ash Reshteh",
+        korteBeschrijving: "Stevige Perzische kruidensoep met peulvruchten, noedels en een topping van yoghurt of ui.",
+        afbeelding: "images/recepten/AshReshtehAI.png",
+        url: "recepten.html?id=ashReshteh"
+    },
+
+    kabsa: {
+        titel: "Kabsa",
+        korteBeschrijving: "Aromatische rijstschotel met specerijen, vlees en vaak noten of rozijnen.",
+        afbeelding: "images/recepten/KabsaAI.png",
+        url: "recepten.html?id=kabsa"
+    },
+
+    mandi: {
+        titel: "Mandi",
+        korteBeschrijving: "Rijstgerecht met mals langzaam gegaard vlees, bekend om zijn rokerige geurige smaak.",
+        afbeelding: "images/recepten/MandiAI.png",
+        url: "recepten.html?id=mandi"
+    },
+
+    harees: {
+        titel: "Harees",
+        korteBeschrijving: "Romig hartig gerecht van tarwe en vlees, langzaam gekookt tot een zachte papachtige structuur.",
+        afbeelding: "images/recepten/HareesAI.png",
+        url: "recepten.html?id=harees"
+    },
+
+    machboos: {
+        titel: "Machboos",
+        korteBeschrijving: "Kruidige rijstschotel uit de Golfregio met vlees of vis, gedroogde limoen en specerijen.",
+        afbeelding: "images/recepten/MachboosAI.png",
+        url: "recepten.html?id=machboos"
+    },
+
+    cevapi: {
+        titel: "Ćevapi",
+        korteBeschrijving: "Gegrilde Balkanworstjes van gekruid gehakt, vaak geserveerd met brood, ui en ajvar.",
+        afbeelding: "images/recepten/CevapiAI.png",
+        url: "recepten.html?id=cevapi"
+    },
+
+    burek: {
+        titel: "Burek",
+        korteBeschrijving: "Bladerdeeggebak gevuld met vlees, kaas, aardappel of spinazie, populair op de Balkan.",
+        afbeelding: "images/recepten/BurekAI.png",
+        url: "recepten.html?id=burek"
+    },
+
+    sarma: {
+        titel: "Sarma",
+        korteBeschrijving: "Rolletjes van kool- of wijnblad gevuld met rijst, kruiden en vaak gehakt.",
+        afbeelding: "images/recepten/SarmaAI.png",
+        url: "recepten.html?id=sarma"
+    },
+
+    ajvar: {
+        titel: "Ajvar",
+        korteBeschrijving: "Smeuïge paprikaspread van geroosterde rode pepers, vaak gegeten als dip of bijgerecht.",
+        afbeelding: "images/recepten/AjvarAI.png",
+        url: "recepten.html?id=ajvar"
+    },
+
+    goulash: {
+        titel: "Goulash",
+        korteBeschrijving: "Stevige stoofschotel met rundvlees, paprika, ui en specerijen uit Midden- en Oost-Europa.",
+        afbeelding: "images/recepten/GoulashAI.png",
+        url: "recepten.html?id=goulash"
+    },
+
+    pierogi: {
+        titel: "Pierogi",
+        korteBeschrijving: "Gevulde deegkussentjes uit Polen, vaak met aardappel, kaas, vlees of zuurkool.",
+        afbeelding: "images/recepten/PierogiAI.png",
+        url: "recepten.html?id=pierogi"
+    },
+
+    svickova: {
+        titel: "Svíčková",
+        korteBeschrijving: "Tsjechisch rundvlees in romige groentesaus, traditioneel geserveerd met knoedels.",
+        afbeelding: "images/recepten/SvickovaAI.png",
+        url: "recepten.html?id=svickova"
+    },
+
+    halusky: {
+        titel: "Halušky",
+        korteBeschrijving: "Zachte Slowaakse deeggnocchi, vaak geserveerd met schapenkaas en spek.",
+        afbeelding: "images/recepten/HaluskyAI.png",
+        url: "recepten.html?id=halusky"
+    },
+
+    gravadLax: {
+        titel: "Gravad Lax",
+        korteBeschrijving: "Dun gesneden zalm, gepekeld met zout, suiker en dille, vaak geserveerd met mosterdsaus.",
+        afbeelding: "images/recepten/GravadLaxAI.png",
+        url: "recepten.html?id=gravadLax"
+    },
+
+    kottbullar: {
+        titel: "Köttbullar",
+        korteBeschrijving: "Zweedse gehaktballetjes, traditioneel geserveerd met jus, aardappelpuree en vossenbessen.",
+        afbeelding: "images/recepten/KottbullarAI.png",
+        url: "recepten.html?id=kottbullar"
+    },
+
+    smorrebrod: {
+        titel: "Smørrebrød",
+        korteBeschrijving: "Deens open roggebrood, rijk belegd met vis, vlees, ei of groenten.",
+        afbeelding: "images/recepten/SmorrebrodAI.png",
+        url: "recepten.html?id=smorrebrod"
+    },
+
+    lutefisk: {
+        titel: "Lutefisk",
+        korteBeschrijving: "Traditioneel Scandinavisch gerecht van gedroogde vis, geweekt en bereid tot een zachte structuur.",
+        afbeelding: "images/recepten/LutefiskAI.png",
+        url: "recepten.html?id=lutefisk"
+    },
+
+    fishAndChips: {
+        titel: "Fish and Chips",
+        korteBeschrijving: "Gefrituurde vis met krokante friet, een klassieker van de Britse eilanden.",
+        afbeelding: "images/recepten/FishAndChipsAI.png",
+        url: "recepten.html?id=fishAndChips"
+    },
+
+    shepherdsPie: {
+        titel: "Shepherd’s Pie",
+        korteBeschrijving: "Ovenschotel met gehakt en groenten, afgedekt met een laag romige aardappelpuree.",
+        afbeelding: "images/recepten/ShepherdsPieAI.png",
+        url: "recepten.html?id=shepherdsPie"
+    },
+
+    fullEnglish: {
+        titel: "Full English",
+        korteBeschrijving: "Uitgebreid Engels ontbijt met eieren, worst, spek, bonen, tomaat en toast.",
+        afbeelding: "images/recepten/FullEnglishAI.png",
+        url: "recepten.html?id=fullEnglish"
+    },
+
+    irishStew: {
+        titel: "Irish Stew",
+        korteBeschrijving: "Ierse stoofpot met lamsvlees of rundvlees, aardappel, ui en wortel.",
+        afbeelding: "images/recepten/IrishStewAI.png",
+        url: "recepten.html?id=irishStew"
+    },
+
+    schnitzel: {
+        titel: "Schnitzel",
+        korteBeschrijving: "Dun uitgeklopt vlees, gepaneerd en goudbruin gebakken, populair in Duitstalige keukens.",
+        afbeelding: "images/recepten/SchnitzelAI.png",
+        url: "recepten.html?id=schnitzel"
+    },
+
+    bratwurst: {
+        titel: "Bratwurst",
+        korteBeschrijving: "Duitse braadworst, vaak geserveerd met mosterd, brood of zuurkool.",
+        afbeelding: "images/recepten/BratwurstAI.png",
+        url: "recepten.html?id=bratwurst"
+    },
+
+    stamppot: {
+        titel: "Stamppot",
+        korteBeschrijving: "Nederlands gerecht van aardappelpuree gemengd met groenten, vaak met rookworst.",
+        afbeelding: "images/recepten/StamppotAI.png",
+        url: "recepten.html?id=stamppot"
+    },
+
+    sauerbraten: {
+        titel: "Sauerbraten",
+        korteBeschrijving: "Gemarineerd en langzaam gegaard rundvlees in een rijke zoetzure saus.",
+        afbeelding: "images/recepten/SauerbratenAI.png",
+        url: "recepten.html?id=sauerbraten"
+    },
+
+    coqAuVin: {
+        titel: "Coq au Vin",
+        korteBeschrijving: "Franse stoofschotel van kip, langzaam gegaard in wijn met spek, ui en paddenstoelen.",
+        afbeelding: "images/recepten/CoqAuVinAI.png",
+        url: "recepten.html?id=coqAuVin"
+    },
+
+    fondue: {
+        titel: "Fondue",
+        korteBeschrijving: "Gesmolten kaasgerecht uit de Alpenregio, gegeten door stukjes brood in de kaas te dippen.",
+        afbeelding: "images/recepten/FondueAI.png",
+        url: "recepten.html?id=fondue"
+    },
+
+    tarteFlambee: {
+        titel: "Tarte Flambée",
+        korteBeschrijving: "Dunne Elzasser deegbodem belegd met room, ui en spek.",
+        afbeelding: "images/recepten/TarteFlambeeAI.png",
+        url: "recepten.html?id=tarteFlambee"
+    },
+
+    wienerSchnitzel: {
+        titel: "Wiener Schnitzel",
+        korteBeschrijving: "Oostenrijks kalfsschnitzel, dun gepaneerd en goudbruin gebakken.",
+        afbeelding: "images/recepten/WienerSchnitzelAI.png",
+        url: "recepten.html?id=wienerSchnitzel"
+    },
+
+    paella: {
+        titel: "Paella",
+        korteBeschrijving: "Spaanse rijstschotel met saffraan, groenten, zeevruchten of vlees.",
+        afbeelding: "images/recepten/PaellaAI.png",
+        url: "recepten.html?id=paella"
+    },
+
+    moussaka: {
+        titel: "Moussaka",
+        korteBeschrijving: "Ovenschotel met aubergine, gehakt en romige bechamelsaus, geliefd in Griekenland.",
+        afbeelding: "images/recepten/MoussakaAI.png",
+        url: "recepten.html?id=moussaka"
+    },
+
+    lasagne: {
+        titel: "Lasagne",
+        korteBeschrijving: "Gelaagde pastaschotel met saus, kaas en vaak ragù of groenten.",
+        afbeelding: "images/recepten/LasagneAI.png",
+        url: "recepten.html?id=lasagne"
+    },
+
+    bacalhau: {
+        titel: "Bacalhau",
+        korteBeschrijving: "Portugees gerecht van gezouten kabeljauw, vaak gecombineerd met aardappel, ui en olijfolie.",
+        afbeelding: "images/recepten/BacalhauAI.png",
+        url: "recepten.html?id=bacalhau"
+    },
+
+    
 
 };
 
@@ -183,17 +605,17 @@ const regioRecepten = {
     "china-oost-azie": ["pekingEend", "mapoTofu", "bibimbap", "xiaolongbao"],
     "japan": ["sushi", "ramen", "tonkatsu", "okonomiyaki"],
     "filipijns": ["adobo", "sinigang", "kareKare", "lumpia"],
-  "zuid-aziatisch": ["biryani", "butterChicken", "dosa", "roganJosh"],
-  "centraal-aziatisch": ["plov", "lagman", "manty", "shashlik"],
-  "kaukasus": ["khachapuri", "khinkali", "dolma", "mtsvadi"],
-  "rus-eurazie": ["borsjtsj", "pelmeni", "blini", "beefStroganoff"],
+    "zuid-aziatisch": ["biryani", "masalaDosa", "momo", "kottuRoti"],
+    "centraal-aziatisch": ["plov", "lagman", "manty", "shashlik"],
+    "kaukasus": ["khachapuri", "khinkali", "dolma", "mtsvadi"],
+    "rus-eurazie": ["borsjtsj", "pelmeni", "blini", "khorkhog"],
 
-  "anatolie": ["kebab", "manti", "lahmacun", "menemen"],
-  "levant": ["shawarma", "falafel", "hummus", "maqluba"],
-  "perzie": ["chelowKebab", "fesenjan", "ghormehSabzi", "ashReshteh"],
-  "arabisch-schiereiland": ["kabsa", "mandi", "harees", "machboos"],
+    "anatolie": ["kebab", "manti", "lahmacun", "menemen"],
+    "levant": ["shawarma", "falafel", "hummus", "maqluba"],
+    "perzie": ["chelowKebab", "fesenjan", "ghormehSabzi", "ashReshteh"],
+    "arabisch-schiereiland": ["kabsa", "mandi", "harees", "machboos"],
 
-  "balkan": ["cevapi", "burek", "sarma", "ajvar"],
+    "balkan": ["cevapi", "burek", "sarma", "ajvar"],
   "oost-centraal-europa": ["goulash", "pierogi", "svickova", "halusky"],
   "scandinavie": ["gravadLax", "kottbullar", "smorrebrod", "lutefisk"],
   "britse-eilanden": ["fishAndChips", "shepherdsPie", "fullEnglish", "irishStew"],
